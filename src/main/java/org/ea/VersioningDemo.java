d145926d23d19d4eddb8199dcad01219cdacef57
Sanjay Singh <sanjaysingh@ghx.com>
2023-02-08 17:33:05 +0530

    Update VersioningDemo.java

--- src/main/java/org/ea/VersioningDemo.java
+++ src/main/java/org/ea/VersioningDemo.java
@@ -7,7 +7,7 @@ public class VersioningDemo {
     public static void main(String args[]) {
         System.out.println("Hello explorers.");
         System.out.println();
-        System.out.println("This program is running version "+
+        System.out.println("Sanjay This program is running version "+
                 VersioningDemo.class.getPackage().getImplementationVersion());
     }
 }
